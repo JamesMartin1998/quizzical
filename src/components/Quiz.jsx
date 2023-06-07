a589eddf016a9956bffb1c4e22bdccb29437c542
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-06-07 17:33:28 +0100

    fetch the questions data when the quiz loads

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,19 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const Quiz = () => {
+
+    const [questionsData, setQuestionsData] = useState([])
+
+    useEffect(() => {
+        const getQuestions = async () => {
+            const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+            const data = await res.json()
+            setQuestionsData(data.results)
+            console.log(data.results)
+        }
+        getQuestions()
+    }, [])
+
     return (
         <div className="Quiz--container">
             test
