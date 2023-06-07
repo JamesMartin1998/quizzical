3b9db7ea4d42b1d5c25dcb9d34b6ce4be89f6645
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-06-07 16:23:34 +0100

    add StartGame component and basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import StartGame from "./components/StartGame"
 
 const App = () => {
   return (
-    <div>
-      Quizzical App
+    <div className="App--container">
+      <div id="App--yellow-circle" className="App--circle-design"></div>
+      <div id="App--blue-circle" className="App--circle-design"></div>
+      <StartGame />
     </div>
   )
 }
---
+++ src/components/StartGame.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const StartGame = () => {
+    return (
+        <div className="StartGame--container">
+            <h1>Quizzical</h1>
+            <p>A quiz game built with React</p>
+            <button>
+                Start Quiz
+            </button>
+        </div>
+    )
+}
+
+export default StartGame
